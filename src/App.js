d6795fd7df123c306da4edaab06883b9e601666c
Jamie <jamiekozminska@gmail.com>
2022-01-19 15:17:36 +1100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { useState } from 'react';
+
+const api = {
+ key: "408107a03cf63bd0123bad5e4f7b0d7a",
+ base: "https://api.openweathermap.org/data/2.5/"
+}
+
+let today = new Date().toDateString();
+
 function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch (`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      .then(res => res.json())
+      .then(result => {
+        setWeather(result);
+        setQuery('');
+        console.log(result);
+      });
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={typeof weather.main != "undefined" ? (weather.main.temp > 16) ? 'app warm' : 'app cool' : 'app'}>
+      <main>
+        <div className="search-box">
+          <input 
+          type="text" 
+          className="search-bar" 
+          placeholder="Search..."
+          onChange={e => setQuery(e.target.value)}
+          value = {query}
+          onKeyPress={search}
+          ></input>
+        </div>
+        {(typeof weather.main != "undefined") ? (
+          <div>
+            <div className='location-box'>
+              <div className='location'>{weather.name}, {weather.sys.country}</div>
+              <div className='date'>{today}</div>
+            </div>
+            <div className='weather-box'>
+              <div className='temp'>{Math.round(weather.main.temp)}°C
+              <div className='temp-lowhigh'>low: {Math.round(weather.main.temp_min)} high: {Math.round(weather.main.temp_max)} </div>
+              </div>
+              <div className='weather'>{weather.weather[0].main}</div>
+            </div>
+          </div>
+        ) : ("") }
+      </main>
     </div>
   );
 }
